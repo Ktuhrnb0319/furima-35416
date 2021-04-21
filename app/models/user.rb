5ab832cbeea58ab81cec00c5cf483d31f55a5c0a
Ktuhrnb0319 <ktuhrnb@gmail.com>
2021-04-21 16:14:25 +0900

    rubocopでの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
-  validates :nickname,presence: true
-  validates :first_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters" }
-  validates :last_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters" }
-  validates :first_furigana,presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
-  validates :last_furigana,presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
-  validates :birth_date,presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  validates :last_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  validates :birth_date, presence: true
 end
