5889babb9059c5c63915a8399b196156500d202d
Ktuhrnb0319 <ktuhrnb@gmail.com>
2021-04-22 12:50:09 +0900

    商品出品機能の導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.all.includes(:user)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
@@ -10,7 +26,7 @@ class ItemsController < ApplicationController
 
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image,:title,:description,:category_id,:city_id,:days_to_delivery_id,:delivery_charge_id,:product_state_id,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,20 +4,23 @@ class Item < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :product_state_id, numericality: { other_than: 1 }
-  validates :delivery_charge_id, numericality: { other_than: 1 }
-  validates :city_id, numericality: { other_than: 1 }
-  validates :days_to_delivery_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 ,message: 'Select'}
+  validates :product_state_id, numericality: { other_than: 1 ,message: 'Select' }
+  validates :delivery_charge_id, numericality: { other_than: 1 ,message: 'Select' }
+  validates :city_id, numericality: { other_than: 1 ,message: 'Select' }
+  validates :days_to_delivery_id, numericality: { other_than: 1 ,message: 'Select' }
   validates :price, presence: true
   validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :city
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :days_to_delivery
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery_charge
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :product_state
 
 end
-
